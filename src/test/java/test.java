ee6dd159c0b23114f2bfcef8fb4accba06a15843
jinkshower <askthem@naver.com>
2023-10-23 13:35:23 +0900

    unit testing basic

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,6 +1,8 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import Junit.BootCamp;
+import Junit.Grader;
+import Junit.SimpleCalculator;
 import org.junit.jupiter.api.Test;
 
 public class test {
@@ -17,5 +19,35 @@ public class test {
     @Test
     public void totalIsValid() {
         assertEquals(113, BootCamp.getTotal(100, 13));
+    }
+
+    @Test
+    void twoPlusTwoShouldEqualFour() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        assertEquals(4,
+                calculator.add(2, 2));
+//        assertTrue(calculator.add(2,2) == 4);
+    }
+    @Test
+    void threePlusSevenShouldEqualTen() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        assertEquals(10,
+                calculator.add(3, 7));
+    }
+
+    @Test
+    void fiftyNineShouldReturnF() {
+        var grader = new Grader();
+        assertEquals('F', grader.determineLetterGrade(59));
+    }
+
+    @Test
+    void negativeOneShouldReturnIllegalArgument() {
+        var grader = new Grader();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    grader.determineLetterGrade(-1);
+                });
+
     }
 }
