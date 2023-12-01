ea007238ebe5a504a70c4994665858737d0c83cf
jinkshower <askthem@naver.com>
2023-12-01 11:41:02 +0900

    moja q2_1

---
+++ src/main/java/ModernJavaInAction/Quiz2_1.java
@@ -0,0 +1,56 @@
+package ModernJavaInAction;
+
+import java.util.List;
+
+public class Quiz2_1 {
+    public static void main(String[] args) {
+        prettyPrintApple(inventory, new AppleSimpleFormatter());
+    }
+
+    public static void prettyPrintApple(List<Apple> inventory, AppleFormatter appleFormatter) {
+        for (Apple apple : inventory) {
+            String output = appleFormatter.accept(apple);
+            System.out.println(output);
+        }
+    }
+
+    class Apple {
+        String name;
+        String color;
+        int weight;
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getColor() {
+            return color;
+        }
+    }
+
+    interface AppleFormatter {
+        String accept(Apple apple);
+    }
+
+    class AppleFancyFormatter implements AppleFormatter {
+
+        @Override
+        public String accept(Apple apple) {
+            String characteristic = apple.getWeight() > 150 ? "heavy" :
+                    "light";
+            return "A" + characteristic + " " + apple.getColor() + " apple";
+        }
+    }
+
+    class AppleSimpleFormatter implements AppleFormatter {
+
+        @Override
+        public String accept(Apple apple) {
+            return "An apple of " + apple.getWeight() + "g";
+        }
+    }
+}
