58a53b69ee281856351cb26b611f1016bcf272a6
jinkshower <askthem@naver.com>
2024-02-17 21:55:36 +0900

    모험가 다시풀기

--- src/main/java/ndb/Adventurers.java
+++ src/main/java/ndb/Adventurers.java
@@ -1,6 +1,9 @@
 package ndb;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Adventurers {
@@ -29,5 +32,31 @@ public class Adventurers {
             }
         }
         System.out.println(answer);
+    }
+
+    //다시 풀기
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+
+        int count = 0;
+        int index = 0;
+        while (true) {
+            int current = list.get(index);
+            index += current;
+            if (index < list.size()) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(count);
     }
 }
