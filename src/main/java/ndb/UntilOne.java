7ef985da37e38e56eff17bbe102dd17ff0cea6b9
jinkshower <askthem@naver.com>
2024-02-17 15:24:13 +0900

    1이 될때까지 다시풀기

--- src/main/java/ndb/UntilOne.java
+++ src/main/java/ndb/UntilOne.java
@@ -13,12 +13,33 @@ public class UntilOne {
         while (true) {
             int target = (n / k) * k;
             count += n - target;
+            n = target;
 
-            if (n < k) break;
+            if (n < k) {
+                break;
+            }
             count++;
             n /= k;
         }
         count += (n - 1);
+        System.out.println(count);
+    }
+
+    //다시 풀기
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int count = 0;
+        while (n != 1) {
+            if (n % k == 0) {
+                n /= k;
+            } else {
+                n -= 1;
+            }
+            count++;
+        }
         System.out.println(count);
     }
 }
