1d6a740bae5d346b7c8a333884596befac9c5fed
jinkshower <askthem@naver.com>
2024-02-17 20:08:00 +0900

    바닥공사 다시풀기

--- src/main/java/ndb/Tile.java
+++ src/main/java/ndb/Tile.java
@@ -18,6 +18,20 @@ public class Tile {
             d[i] = (d[i - 1] + d[i - 2] * 2) % 796796;
         }
         System.out.println(d[n]);
-
     }
+
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] d = new int[n + 1];
+        d[0] = 1;
+        d[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            d[i] = (d[i - 1] + d[i - 2] * 2) % 796796;
+        }
+
+        System.out.println(d[n]);
+    }v
 }
