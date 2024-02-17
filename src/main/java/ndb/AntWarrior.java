948a1fe582bae4ffcd752adb00de39e5f5b10bf4
jinkshower <askthem@naver.com>
2024-02-17 19:57:09 +0900

    개미전사 다시풀기

--- src/main/java/ndb/AntWarrior.java
+++ src/main/java/ndb/AntWarrior.java
@@ -23,4 +23,23 @@ public class AntWarrior {
         }
         System.out.println(d[n - 1]);
     }
+
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] d = new int[n + 1];
+        d[0] = 0;
+        d[1] = arr[1];
+
+        for (int i = 2; i <= n; i++) {
+            d[i] = Math.max(d[i - 1], d[i - 2] + arr[i]);
+        }
+        System.out.println(d[n]);
+    }
 }
