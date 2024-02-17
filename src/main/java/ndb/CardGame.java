9a81d97fbac2ac82cb549d1b85104fe26ccbcbd4
jinkshower <askthem@naver.com>
2024-02-17 14:59:11 +0900

    카드게임 다시 풀기

--- src/main/java/ndb/CardGame.java
+++ src/main/java/ndb/CardGame.java
@@ -3,6 +3,7 @@ package ndb;
 import java.util.Scanner;
 
 public class CardGame {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -19,6 +20,25 @@ public class CardGame {
             }
             max = Math.max(max, min_value);
         }
+        System.out.println(max);
+    }
+
+    //다시 풀기
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < m; j++) {
+                int current = sc.nextInt();
+                min = Math.min(min, current);
+            }
+            max = Math.max(min, max);
+        }
+
         System.out.println(max);
     }
 }
