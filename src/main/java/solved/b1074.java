248be8738ec4e79b85d266be608ceb4b49681573
jinkshower <askthem@naver.com>
2024-02-19 10:28:03 +0900

    z 다시 풀기

--- src/main/java/solved/b1074.java
+++ src/main/java/solved/b1074.java
@@ -43,4 +43,40 @@ public class b1074 {
 
         return z(--n, x, y, result);
     }
+
+    //다시 풀기
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int result = solve(n, r, c, 0);
+        System.out.println(result);
+    }
+
+    static int solve(int n, int r, int c, int result) {
+        if (n == 0) {
+            return result;
+        }
+        int half = (int) Math.pow(2, n) / 2 - 1;
+
+        int quad;
+        if (r > half && c > half) {
+            r -= half;
+            c -= half;
+            quad = 3;
+        } else if (r > half && c <= half) {
+            r -= half;
+            quad = 2;
+        } else if (r <= half && c > half) {
+            c -= half;
+            quad = 1;
+        } else {
+            quad = 0;
+        }
+        result += (int) Math.pow(4, n - 1) * quad;
+
+        return solve(--n, r, c, result);
+    }
 }
