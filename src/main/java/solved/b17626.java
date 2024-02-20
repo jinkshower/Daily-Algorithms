1f64d0f88af5114d4647ce78f410ecb4eb84352c
jinkshower <askthem@naver.com>
2024-02-20 20:26:06 +0900

    four squares 다시 풀기

--- src/main/java/solved/b17626.java
+++ src/main/java/solved/b17626.java
@@ -23,4 +23,23 @@ public class b17626 {
         }
         System.out.println(d[n]);
     }
+
+    //다시풀기
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] d = new int[n + 1];
+        d[0] = 0;
+        d[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            d[i] = d[i - 1];
+            for (int j = 2; j * j <= i; j++) {
+                d[i] = Math.min(d[i - j * j], d[i]);
+            }
+            d[i] += 1;
+        }
+        System.out.println(d[n]);
+    }
 }
