ab620f4e1e942e888c3d627651fd22beb76c3145
jinkshower <askthem@naver.com>
2024-02-21 21:17:23 +0900

    매출액의 종류

---
+++ src/main/java/javaCourse/j4to3.java
@@ -0,0 +1,42 @@
+package javaCourse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class j4to3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < k - 1; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>();
+        int lt = 0;
+        for (int rt = k - 1; rt < n; rt++) {
+            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+            list.add(map.size());
+            map.put(arr[lt], map.get(arr[lt]) - 1);
+            if (map.get(arr[lt]) == 0) {
+                map.remove(arr[lt]);
+            }
+            lt++;
+        }
+
+        for (int i : list) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
