2921b6c40cfcfb73bafac57b7a38831cb08f6f4d
jinkshower <askthem@naver.com>
2024-02-25 11:42:15 +0900

    대소문자 변환

---
+++ src/main/java/javaCourse/j1to2.java
@@ -0,0 +1,21 @@
+package javaCourse;
+
+import java.util.Scanner;
+
+public class j1to2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if (c >= 97 && c <= 122) {
+                sb.append((char) (c - 32));
+            } else {
+                sb.append((char) (c + 32));
+            }
+        }
+        System.out.println(sb);
+    }
+}
