01460cada3499331d906ecb5f371fb8f541f5f18
jinkshower <askthem@naver.com>
2024-02-25 12:12:20 +0900

    문장속 단어 index of substring으로 풀기

--- src/main/java/javaCourse/string문자열1강/j1to3.java
+++ src/main/java/javaCourse/string문자열1강/j1to3.java
@@ -21,4 +21,28 @@ public class j1to3 {
         }
         System.out.println(answer);
     }
+
+    //indexOf, substring으로 풀기
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        int max = Integer.MIN_VALUE;
+        String answer = "";
+        int pos = 0;
+        while ((pos = str.indexOf(' ')) != -1) {
+            String tmp = str.substring(0, pos);
+            if (tmp.length() > max) {
+                max = tmp.length();
+                answer = tmp;
+            }
+            str = str.substring(pos + 1);
+        }
+        if (str.length() > max) {
+            answer = str;
+        }
+        System.out.println(answer);
+    }
 }
