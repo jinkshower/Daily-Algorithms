1edea37920f73602f8f2313f79620b6ab46b87cb
jinkshower <askthem@naver.com>
2024-02-27 09:49:02 +0900

    tree 말단 노드까지 가장짧은 경로(bfs)

---
+++ src/main/java/javaCourse/RecursiveTreeGraph7강/j7to10.java
@@ -0,0 +1,20 @@
+package javaCourse.RecursiveTreeGraph7강;
+
+public class j7to10 {
+//    static int bfs(Node root) {
+//        Queue<Node> q = new LinkedList<>();
+//        q.offer(root);
+//        int L = 0;
+//        while (!q.isEmpty()) {
+//            int len = q.size();
+//            for (int i = 0 ; i < len; i++) {
+//                Node cur = q.poll();
+//                if (cur.lt == null && cur.rt == null) return L;
+//                if (cur.lt != null) q.offer(cur.lt);
+//                if (cur.rt != null) q.offer(cur.rt);
+//            }
+//            L++;
+//        }
+//        return -1;
+//    }
+}
