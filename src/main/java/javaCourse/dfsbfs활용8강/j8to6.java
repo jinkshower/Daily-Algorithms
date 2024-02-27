fcd8ee8672c74586a2e415731bc7105ba6715ec7
jinkshower <askthem@naver.com>
2024-02-27 22:26:09 +0900

    순열

---
+++ src/main/java/javaCourse/dfsbfs활용8강/j8to6.java
@@ -0,0 +1,40 @@
+package javaCourse.dfsbfs활용8강;
+
+import java.util.Scanner;
+
+public class j8to6 {
+
+    static int[] ch, pm, arr;
+    static int n, m;
+
+    static void dfs(int L) {
+        if (L == m) {
+            for (int x : pm) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    pm[L] = arr[i];
+                    dfs(L + 1);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        ch = new int[n];
+        pm = new int[n];
+        dfs(0);
+    }
+}
