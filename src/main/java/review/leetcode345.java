d4fc9110864e5b1f18c8c8b6a229e2099428e81e
jinkshower <askthem@naver.com>
2024-07-16 22:44:26 +0900

    LeetCode345. Reverse Vowels of a String 알고리즘: 투포인터(2번째 풀이) O(n) 자료구조: 스택(1번째 풀이) O(2n) 사고과정 1. 문자열 + 제한이 커서 스택을 먼저 떠오름. 모음을 확인하고 스택에 넣고 pop하면서 문자열을 완성시키면 된다고 생각함.
    2. n번의 탐색이 2번 일어나서 더 효율적인 방법을 찾아봄. 투포인터로 left , right 탐색하고 모음나올때까지 left는 증가 right는 감소해서 둘을 바꿔주면 되겠다고 생각함.
    
    3. ".,"와 같은 값+ 모음이 다 바뀌고 left, right가 계속 증가, 감소하는 문제가 있어서 제한 사항을 추가함.

---
+++ src/main/java/review/leetcode345.java
@@ -0,0 +1,55 @@
+package review;
+
+public class leetcode345 {
+
+	public String reverseVowels(String s) {
+		//스택풀이
+		// char[] result = new char[s.length()];
+		// Stack<Character> stack = new Stack<>();
+
+		// for (char c : s.toCharArray()) {
+		//     if (isVowel(c)) {
+		//         stack.push(c);
+		//     }
+		// }
+
+		// for (int i = 0; i < s.length(); i++) {
+		//     char cur = s.charAt(i);
+		//     if (isVowel(cur)) {
+		//         result[i] = stack.pop();
+		//         continue;
+		//     }
+		//     result[i] = cur;
+		// }
+		char[] chars = s.toCharArray();
+		int left = 0;
+		int right = s.length() - 1;
+
+		while (left < right) {
+			while (left < s.length() && !isVowel(chars[left])) {
+				left++;
+			}
+
+			while (right >= 0 && !isVowel(chars[right])) {
+				right--;
+			}
+
+			if (left >= right) {
+				break;
+			}
+			char tmp = ' ';
+			tmp = chars[left];
+			chars[left] = chars[right];
+			chars[right] = tmp;
+			left++;
+			right--;
+		}
+
+		return new String(chars);
+	}
+
+	private boolean isVowel(char c) {
+		c = Character.toLowerCase(c);
+		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+	}
+}
