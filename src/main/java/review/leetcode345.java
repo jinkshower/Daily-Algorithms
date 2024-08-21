aa2beed5419033f16c214ef79143aa9e80614db7
jinkshower <askthem@naver.com>
2024-08-21 09:42:06 +0900

    복습 및 다시 품

--- src/main/java/review/leetcode345.java
+++ src/main/java/review/leetcode345.java
@@ -1,55 +1,55 @@
 package review;
 
 public class leetcode345 {
+    //8/21 leetcode75 다시 품
+    public String reverseVowels(String s) {
+        //스택풀이
+        // char[] result = new char[s.length()];
+        // Stack<Character> stack = new Stack<>();
 
-	public String reverseVowels(String s) {
-		//스택풀이
-		// char[] result = new char[s.length()];
-		// Stack<Character> stack = new Stack<>();
+        // for (char c : s.toCharArray()) {
+        //     if (isVowel(c)) {
+        //         stack.push(c);
+        //     }
+        // }
 
-		// for (char c : s.toCharArray()) {
-		//     if (isVowel(c)) {
-		//         stack.push(c);
-		//     }
-		// }
+        // for (int i = 0; i < s.length(); i++) {
+        //     char cur = s.charAt(i);
+        //     if (isVowel(cur)) {
+        //         result[i] = stack.pop();
+        //         continue;
+        //     }
+        //     result[i] = cur;
+        // }
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = s.length() - 1;
 
-		// for (int i = 0; i < s.length(); i++) {
-		//     char cur = s.charAt(i);
-		//     if (isVowel(cur)) {
-		//         result[i] = stack.pop();
-		//         continue;
-		//     }
-		//     result[i] = cur;
-		// }
-		char[] chars = s.toCharArray();
-		int left = 0;
-		int right = s.length() - 1;
+        while (left < right) {
+            while (left < s.length() && !isVowel(chars[left])) {
+                left++;
+            }
 
-		while (left < right) {
-			while (left < s.length() && !isVowel(chars[left])) {
-				left++;
-			}
+            while (right >= 0 && !isVowel(chars[right])) {
+                right--;
+            }
 
-			while (right >= 0 && !isVowel(chars[right])) {
-				right--;
-			}
+            if (left >= right) {
+                break;
+            }
+            char tmp = ' ';
+            tmp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = tmp;
+            left++;
+            right--;
+        }
 
-			if (left >= right) {
-				break;
-			}
-			char tmp = ' ';
-			tmp = chars[left];
-			chars[left] = chars[right];
-			chars[right] = tmp;
-			left++;
-			right--;
-		}
+        return new String(chars);
+    }
 
-		return new String(chars);
-	}
-
-	private boolean isVowel(char c) {
-		c = Character.toLowerCase(c);
-		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-	}
+    private boolean isVowel(char c) {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
 }
