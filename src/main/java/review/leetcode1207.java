4de078820d76464c4e0111b7211f52c349b389fb
jinkshower <askthem@naver.com>
2024-09-10 10:49:23 +0900

    복기 x

--- src/main/java/review/leetcode1207.java
+++ src/main/java/review/leetcode1207.java
@@ -7,13 +7,22 @@ import java.util.Set;
 
 public class leetcode1207 {
 
-	public boolean uniqueOccurrences(int[] arr) {
-		Map<Integer, Integer> hash = new HashMap<>();
-		for (int i = 0; i < arr.length; i++) {
-			hash.put(arr[i], hash.getOrDefault(arr[i], 0) + 1);
-		}
-		int size = hash.values().size();
-		Set<Integer> set = new HashSet<>(hash.values());
-		return size == set.size();
-	}
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            hash.put(arr[i], hash.getOrDefault(arr[i], 0) + 1);
+        }
+        int size = hash.values().size();
+        Set<Integer> set = new HashSet<>(hash.values());
+        return size == set.size();
+    }
+
+    public boolean uniqueOccurrences2(int[] arr) {
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i : arr) {
+            hash.put(i, hash.getOrDefault(i, 0) + 1);
+        }
+        Set<Integer> set = new HashSet<>(hash.values());
+        return set.size() == hash.size();
+    }
 }
