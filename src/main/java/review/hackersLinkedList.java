7a075297b9c92b51de6d5791d79254c2fa6c7c91
jinkshower <askthem@naver.com>
2024-10-15 22:16:17 +0900

    해커스 링크드 리스트 중간에 삽입 position 주어짐

--- src/main/java/review/hackersLinkedList.java
+++ src/main/java/review/hackersLinkedList.java
@@ -29,4 +29,33 @@ public class hackersLinkedList {
         newNode.next = llist;
         return newNode;
     }
+
+    public static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position) {
+        // Write your code here
+        SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+
+        if (llist == null) {
+            llist = newNode;
+            return llist;
+        }
+
+        if (position == 0) {
+            newNode.next = llist;
+            return newNode;
+        }
+
+        SinglyLinkedListNode current = llist;
+        for (int i = 0; i < position - 1; i++) {
+            if (current.next == null) {
+                break;
+            }
+            current = current.next;
+        }
+
+        newNode.next = current.next;
+        current.next = newNode;
+
+        return llist;
+    }
+
 }
