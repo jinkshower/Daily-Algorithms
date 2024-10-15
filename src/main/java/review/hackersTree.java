14461fdfc39d1b20ff7c45e868ae051304e1545b
jinkshower <askthem@naver.com>
2024-10-16 08:58:46 +0900

    해커스 트리 중위순회

--- src/main/java/review/hackersTree.java
+++ src/main/java/review/hackersTree.java
@@ -20,4 +20,14 @@ public class hackersTree {
         }
         System.out.print(root.data + " ");
     }
+
+    public static void inOrder(Node root) {
+        if (root.left != null) {
+            inOrder(root.left);
+        }
+        System.out.print(root.data + " ");
+        if (root.right != null) {
+            inOrder(root.right);
+        }
+    }
 }
