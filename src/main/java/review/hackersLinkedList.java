17b07759d59fb7ae3671ca8d294bd378eb21eabf
jinkshower <askthem@naver.com>
2024-10-15 22:23:53 +0900

    해커스 링크드 리스트 중간 삭제

--- src/main/java/review/hackersLinkedList.java
+++ src/main/java/review/hackersLinkedList.java
@@ -58,4 +58,22 @@ public class hackersLinkedList {
         return llist;
     }
 
+    public static SinglyLinkedListNode deleteNode(SinglyLinkedListNode llist, int position) {
+        // Write your code here
+        if (position == 0) {
+            llist = llist.next;
+            return llist;
+        }
+
+        SinglyLinkedListNode current = llist;
+        for (int i = 0; i < position - 1; i++) {
+            if (current.next == null) {
+                break;
+            }
+            current = current.next;
+        }
+
+        current.next = current.next.next;
+        return llist;
+    }
 }
