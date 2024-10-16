58028c8d4613df0d3c04453bd239d771d5d386d5
jinkshower <askthem@naver.com>
2024-10-16 09:08:52 +0900

    해커스 트리 깊이 구하기

--- src/main/java/review/hackersTree.java
+++ src/main/java/review/hackersTree.java
@@ -30,4 +30,16 @@ public class hackersTree {
             inOrder(root.right);
         }
     }
+
+    public static int height(Node root) {
+        // Write your code here.
+        if (root == null) {
+            return -1;// 간선의 개수이므로. 노드의 개수라면 0을 리턴해야함.
+        }
+
+        int leftCount = height(root.left);
+        int rightCount = height(root.right);
+
+        return Math.max(leftCount, rightCount) + 1;
+    }
 }
