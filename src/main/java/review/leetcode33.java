36510c850314e88ec218e1f7fafc0e9b5455ef82
jinkshower <askthem@naver.com>
2024-10-22 21:52:01 +0900

    LeetCode33. Search in Rotated Sorted Array 알고리즘: Binary Search 복잡도: O(log n) 사고과정 1. 문제의 log n의 알고리즘이 필요하다는 힌트는 이진탐색을 사용하라는 의미로 해석했음. 이때 rotate 된 배열의 그래프를 그려보는 게 도움이 됨 2. 어느 시점에서 올라가다 끊기고 다시 올라가는 형태인데 이때 한쪽은 '정렬'된 상태이기 때문에 target이 이 정렬된 상태 안에 있다면 범위를 줄일 수 있는 근거가 됨 3. 따라서 m으로 가운데를 찍어보고 왼쪽, 오른쪽 정렬을 판단한 뒤에 target의 범위를 잡아보고 l, r을 움직임. 정렬을 판단한 후 범위를 탐색해서 target이 없다면 이 또한 범위를 줄이는 근거가 됨.

---
+++ src/main/java/review/leetcode33.java
@@ -0,0 +1,31 @@
+package review;
+
+public class leetcode33 {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            //찾았다면 가운데 인덱스 반환
+            if (nums[m] == target) {
+                return m;
+            }
+
+            if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
