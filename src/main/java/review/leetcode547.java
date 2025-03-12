d81bc710deb20822b976dc9c6c0f35c028cc174e
jinkshower <askthem@naver.com>
2025-03-12 20:52:09 +0900

    Leetcode 547. number of province
    복잡도: o(n^2) 완탐 dfs
    사고과정
    복습 복습

--- src/main/java/review/leetcode547.java
+++ src/main/java/review/leetcode547.java
@@ -25,4 +25,32 @@ public class leetcode547 {
             }
         }
     }
+
+    public int findCircleNum2(int[][] isConnected) {
+        // n <= 20
+        // 1 1 0
+        // 1 1 0
+        // 0 0 1
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs2(i, isConnected, visited);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void dfs2(int i, int[][] isConnected, boolean[] visited) {
+        visited[i] = true;
+
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                dfs2(j, isConnected, visited);
+            }
+        }
+    }
 }
